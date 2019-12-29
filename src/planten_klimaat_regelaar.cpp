2cb8898abb2a9ad8cb54b871de5c2e308f72db03
Wielders <camiel.wielders@thermofisher.com>
2019-12-29 23:10:08 +0100

    invooer settings een end op streek

--- src/planten_klimaat_regelaar.cpp
+++ src/planten_klimaat_regelaar.cpp
@@ -795,7 +795,10 @@ class TouchScreen {
     }
 
     void toonStartScherm(String myDatumTijd) {
+        Serial.println("begin toonstartscherm");
         currentPage = 1;
+        Serial.print("currentPage = ");
+        Serial.println(currentPage);
         myGLCD.clrScr();
         for (int i = 0; i < 3; i++) {
             switch (klimaatDataNu[i][SEIZOEN]) {
@@ -850,19 +853,18 @@ class TouchScreen {
             myGLCD.setColor(VGA_YELLOW);
             myGLCD.print(String(klimaatDataNu[i][TEMPERATUUR]), 28, (i*71) + 18);
             myGLCD.print(String("C"), 60, (i*71) + 18);
-             myGLCD.fillCircle(61, (i*71) + 19, 2);
+            myGLCD.fillCircle(61, (i*71) + 19, 2);
             myGLCD.print(String(klimaatDataNu[i][LUCHTVOCHTIGHEIDNU]) + "%", 5, (i*71) + 51);
             myGLCD.print(String(klimaatDataNu[i][POTVOCHTIGHEID]) + "%", 140, (i*71) + 18);
             myGLCD.print(String(klimaatDataNu[i][LICHT]) + "%", 140, (i*71) + 51);
-            }
+        }
         myGLCD.setColor(VGA_WHITE);
         myGLCD.setBackColor(VGA_BLACK);
         myGLCD.setFont(BigFont);
         myGLCD.print(myDatumTijd, CENTER, 220);
     }
 
-    void tekenSettingsScherm(int bak) {
-        currentPage = 2;
+    void tekenSettingsOverzicht(int bak) {
         myGLCD.clrScr();
         for (int i = 0; i < 3; i++) {
             if(i == 0) {
@@ -1012,9 +1014,42 @@ class TouchScreen {
                 }
                 myGLCD.setColor(VGA_WHITE);
                 myGLCD.drawRoundRect((j*26) + 4, (i*71) + 29, (j*26) + 30, (i*71) + 42);
+            }          
+        } 
+    }
+
+    void tekenSettingsManipulatieScherm (int bak) {
+        tekenSettingsOverzicht(bak);
+        int variable;
+        currentPage = 3;
+                Serial.print("currentPage = ");
+        Serial.println(currentPage);
+        while (currentPage == 3) {
+            Serial.println("while loop kiessettings");
+            Serial.print("currentPage = ");
+            Serial.println(currentPage);
+            if (currentPage == 3 && myTouch.dataAvailable()) {
+                myTouch.read();
+                x=myTouch.getX();
+                y=myTouch.getY();
+                for(int i=0;i<3;i++) {
+                    int seizoen = i;
+                    if ((y<=(i*71) + 29) && (y>=(i*71) + 13) && (x>=29 && x<=61)) { 
+                    drawButtons();
+                    variable = NACHTTEMPERATUUR;
+                    leesGetal(bak, seizoen, variable);
+                    }
+                }
             }
-          
-        }  
+        }
+    }
+
+    void tekenSettingsScherm(int bak) {
+        tekenSettingsOverzicht(bak);
+    Serial.println("begin tekenSettingsscherm");
+    currentPage = 2;
+    Serial.print("currentPage = ");
+    Serial.println(currentPage);
         myGLCD.setColor(VGA_BLACK);
         myGLCD.setBackColor(VGA_BLACK);
         myGLCD.fillRoundRect(2, 215, 80, 238);
@@ -1029,256 +1064,212 @@ class TouchScreen {
         myGLCD.print("Back", 266, 221);
         myGLCD.print("Settings", 10, 221);
         while (currentPage == 2) {
-            kiesFunctie(bak);
-        } 
-    }
-
-    void kiesSettings(int bak) {
-        int variable;
-        if (currentPage == 3 && myTouch.dataAvailable()) {
-            myTouch.read();
-            x=myTouch.getX();
-            y=myTouch.getY();
-            for(int i=0;i<3;i++) {
-
-           
-                if ((y<=(i*71) + 29) && (y>=(i*71) + 13) && (x>=29 && x<=61)) { 
-                drawButtons();
-                variable = NACHTTEMPERATUUR;
-                leesGetal(bak, i, variable);
-                }
-            }
-        }
-    }
-
-void drawButtons() {
-// Draw the upper row of buttons
-    currentPage = 4;
-    myGLCD.clrScr();
-    myGLCD.setFont(BigFont);
-     myGLCD.setBackColor(VGA_BLUE);
-    for (x=0; x<5; x++)
-    {
-        myGLCD.setColor(0, 0, 255);
-        myGLCD.fillRoundRect (10+(x*60), 10, 60+(x*60), 60);
-        myGLCD.setColor(255, 255, 255);
-        myGLCD.drawRoundRect (10+(x*60), 10, 60+(x*60), 60);
-        myGLCD.printNumI(x+1, 27+(x*60), 27);
-    }
-// Draw the center row of buttons
-    for (x=0; x<5; x++)
-    {
-        myGLCD.setColor(0, 0, 255);
-        myGLCD.fillRoundRect (10+(x*60), 70, 60+(x*60), 120);
-        myGLCD.setColor(255, 255, 255);
-        myGLCD.drawRoundRect (10+(x*60), 70, 60+(x*60), 120);
-        if (x<4)
-        myGLCD.printNumI(x+6, 27+(x*60), 87);
-    }
-    myGLCD.print("0", 267, 87);
-    // Draw the lower row of buttons
-    myGLCD.setColor(0, 0, 255);
-    myGLCD.fillRoundRect (10, 130, 150, 180);
-    myGLCD.setColor(255, 255, 255);
-    myGLCD.drawRoundRect (10, 130, 150, 180);
-    myGLCD.print("Clear", 40, 147);
-    myGLCD.setColor(0, 0, 255);
-    myGLCD.fillRoundRect (160, 130, 300, 180);
-    myGLCD.setColor(255, 255, 255);
-    myGLCD.drawRoundRect (160, 130, 300, 180);
-    myGLCD.print("Enter", 190, 147);
-    myGLCD.setBackColor (0, 0, 0);
-    }
-
-void updateStr(int val)
-{
-  if (stCurrentLen<20)
-  {
-    stCurrent[stCurrentLen]=val;
-    stCurrent[stCurrentLen+1]='\0';
-    stCurrentLen++;
-    myGLCD.setColor(0, 255, 0);
-    myGLCD.print(stCurrent, LEFT, 224);
-  }
-  else
-  {
-    myGLCD.setColor(255, 0, 0);
-    myGLCD.print("BUFFER FULL!", CENTER, 192);
-    delay(500);
-    myGLCD.print("            ", CENTER, 192);
-    delay(500);
-    myGLCD.print("BUFFER FULL!", CENTER, 192);
-    delay(500);
-    myGLCD.print("            ", CENTER, 192);
-    myGLCD.setColor(0, 255, 0);
-  }
-}
-
-// Draw a red frame while a button is touched
-void waitForIt(int x1, int y1, int x2, int y2)
-{
-  myGLCD.setColor(255, 0, 0);
-  myGLCD.drawRoundRect (x1, y1, x2, y2);
-  while (myTouch.dataAvailable())
-    myTouch.read();
-  myGLCD.setColor(255, 255, 255);
-  myGLCD.drawRoundRect (x1, y1, x2, y2);
-}
-
-
-    void leesGetal(int bak, int seizoen, int variable)
-{
-  while (currentPage ==4)
-  {
-    if (myTouch.dataAvailable())
-    {
-      myTouch.read();
-      x=myTouch.getX();
-      y=myTouch.getY();
-      
-      if ((y>=10) && (y<=60))  // Upper row
-      {
-        if ((x>=10) && (x<=60))  // Button: 1
-        {
-          waitForIt(10, 10, 60, 60);
-          updateStr('1');
-        }
-        if ((x>=70) && (x<=120))  // Button: 2
-        {
-          waitForIt(70, 10, 120, 60);
-          updateStr('2');
-        }
-        if ((x>=130) && (x<=180))  // Button: 3
-        {
-          waitForIt(130, 10, 180, 60);
-          updateStr('3');
-        }
-        if ((x>=190) && (x<=240))  // Button: 4
-        {
-          waitForIt(190, 10, 240, 60);
-          updateStr('4');
-        }
-        if ((x>=250) && (x<=300))  // Button: 5
-        {
-          waitForIt(250, 10, 300, 60);
-          updateStr('5');
-        }
-      }
-
-      if ((y>=70) && (y<=120))  // Center row
-      {
-        if ((x>=10) && (x<=60))  // Button: 6
-        {
-          waitForIt(10, 70, 60, 120);
-          updateStr('6');
-        }
-        if ((x>=70) && (x<=120))  // Button: 7
-        {
-          waitForIt(70, 70, 120, 120);
-          updateStr('7');
-        }
-        if ((x>=130) && (x<=180))  // Button: 8
-        {
-          waitForIt(130, 70, 180, 120);
-          updateStr('8');
-        }
-        if ((x>=190) && (x<=240))  // Button: 9
-        {
-          waitForIt(190, 70, 240, 120);
-          updateStr('9');
-        }
-        if ((x>=250) && (x<=300))  // Button: 0
-        {
-          waitForIt(250, 70, 300, 120);
-          updateStr('0');
-        }
-      }
-
-      if ((y>=130) && (y<=180))  // Upper row
-      {
-        if ((x>=10) && (x<=150))  // Button: Clear
-        {
-          waitForIt(10, 130, 150, 180);
-          stCurrent[0]='\0';
-          stCurrentLen=0;
-          myGLCD.setColor(0, 0, 0);
-          myGLCD.fillRect(0, 224, 319, 239);
-        }
-        if ((x>=160) && (x<=300))  // Button: Enter
-        {
-          waitForIt(160, 130, 300, 180);
-          if (stCurrentLen>0)
-          {
-            for (x=0; x<stCurrentLen+1; x++)
-            {
-              stLast[x]=stCurrent[x];
-            }
-            stCurrent[0]='\0';
-            stCurrentLen=0;
-            myGLCD.setColor(0, 0, 0);
-            myGLCD.fillRect(0, 208, 319, 239);
-            myGLCD.setColor(0, 255, 0);
-            myGLCD.print(stLast, LEFT, 208);
-            int number = atoi(stLast);
-            plantenBakSettings1[bak][seizoen][variable] = number;
-            
-            stCurrent[2]="";
-            stCurrentLen=0;
-            stLast[2]=""; 
-            tekenSettingsScherm(bak); 
-
-          }
-          else
-          {
-            myGLCD.setColor(255, 0, 0);
-            myGLCD.print("BUFFER EMPTY", CENTER, 192);
-            delay(500);
-            myGLCD.print("            ", CENTER, 192);
-            delay(500);
-            myGLCD.print("BUFFER EMPTY", CENTER, 192);
-            delay(500);
-            myGLCD.print("            ", CENTER, 192);
-            myGLCD.setColor(0, 255, 0);
-          }
-        }
-      }
-    }
-  }
-}
-
-    void kiesFunctie(int bak) {
-        String test = "opnieuw";
-        if (currentPage == 2 && myTouch.dataAvailable()) {
-            myTouch.read();
-            x=myTouch.getX();
-            y=myTouch.getY();
-            if ((y>=210) && (y<=240)) { 
-                if ((x>=0) && (x<=80)) {
-                    betast(2, 215, 80, 238);
-                    currentPage = 3;
-                    while (currentPage == 3) {
-                        kiesSettings(bak);
+    Serial.println("while loop settingsscherm");
+    Serial.print("currentPage = ");
+    Serial.println(currentPage);
+            String test = "opnieuw";
+            if (currentPage == 2 && myTouch.dataAvailable()) {
+                myTouch.read();
+                x=myTouch.getX();
+                y=myTouch.getY();
+                if ((y>=210) && (y<=240)) { 
+                    if ((x>=0) && (x<=80)) {
+                        betast(2, 215, 80, 238);
+                        tekenSettingsManipulatieScherm(bak);
                     }
-
-                }
-                if ((x>=80) && (x<=160)) {
-                    betast(82, 215, 160, 238);
-                }
-                if ((x>=160) && (x<=240)) {
-                    betast(162, 215, 240, 238);
-                }
-                if ((x>=240) && (x<=320)) {
-                    betast(242, 215, 318, 238);
-                    toonStartScherm(test);
+                    if ((x>=80) && (x<=160)) {
+                        betast(82, 215, 160, 238);
+                    }
+                    if ((x>=160) && (x<=240)) {
+                        betast(162, 215, 240, 238);
+                    }
+                    if ((x>=240) && (x<=320)) {
+                        betast(242, 215, 318, 238);
+                        toonStartScherm(test);
+                    }
                 }
             }
         }
     }
 
+    void drawButtons() {
+        currentPage = 4;
+    Serial.println("begin drawbuttons");
+    Serial.print("currentPage = ");
+    Serial.println(currentPage);
+        myGLCD.clrScr();
+        myGLCD.setFont(BigFont);
+        myGLCD.setBackColor(VGA_BLUE);
+        for (x=0; x<5; x++) {// Draw the upper row of buttons
+            myGLCD.setColor(0, 0, 255);
+            myGLCD.fillRoundRect (10+(x*60), 10, 60+(x*60), 60);
+            myGLCD.setColor(255, 255, 255);
+            myGLCD.drawRoundRect (10+(x*60), 10, 60+(x*60), 60);
+            myGLCD.printNumI(x+1, 27+(x*60), 27);
+        }
+   
+        for (x=0; x<5; x++) {  // Draw the center row of buttons
+            myGLCD.setColor(0, 0, 255);
+            myGLCD.fillRoundRect (10+(x*60), 70, 60+(x*60), 120);
+            myGLCD.setColor(255, 255, 255);
+            myGLCD.drawRoundRect (10+(x*60), 70, 60+(x*60), 120);
+            if (x<4)
+            myGLCD.printNumI(x+6, 27+(x*60), 87);
+        }
+        myGLCD.print("0", 267, 87); // Draw the lower row of buttons
+        myGLCD.setColor(0, 0, 255);
+        myGLCD.fillRoundRect (10, 130, 150, 180);
+        myGLCD.setColor(255, 255, 255);
+        myGLCD.drawRoundRect (10, 130, 150, 180);
+        myGLCD.print("Clear", 40, 147);
+        myGLCD.setColor(0, 0, 255);
+        myGLCD.fillRoundRect (160, 130, 300, 180);
+        myGLCD.setColor(255, 255, 255);
+        myGLCD.drawRoundRect (160, 130, 300, 180);
+        myGLCD.print("Enter", 190, 147);
+        myGLCD.setBackColor (0, 0, 0);
+    }
+
+    void updateStr(int val) {
+    if (stCurrentLen<20) {
+        stCurrent[stCurrentLen]=val;
+        stCurrent[stCurrentLen+1]='\0';
+        stCurrentLen++;
+        myGLCD.setColor(0, 255, 0);
+        myGLCD.print(stCurrent, LEFT, 224);
+    }   else {
+            myGLCD.setColor(255, 0, 0);
+            myGLCD.print("BUFFER FULL!", CENTER, 192);
+            delay(500);
+            myGLCD.print("            ", CENTER, 192);
+            delay(500);
+            myGLCD.print("BUFFER FULL!", CENTER, 192);
+            delay(500);
+            myGLCD.print("            ", CENTER, 192);
+            myGLCD.setColor(0, 255, 0);
+        }
+    }
+
+    void leesGetal(int bak, int seizoen, int variable) {
+    
+        Serial.println("begin leesgetal");
+        while (currentPage ==4) {
+        Serial.println("while loop leesgetal");
+        Serial.print("currentPage = ");
+        Serial.println(currentPage);
+            if (myTouch.dataAvailable()) {
+                myTouch.read();
+                x=myTouch.getX();
+                y=myTouch.getY();
+                if ((y>=10) && (y<=60)) { // Upper row
+                    if ((x>=10) && (x<=60))  { // Button: 1
+                        betast(10, 10, 60, 60);
+                        updateStr('1');
+                    }
+                    if ((x>=70) && (x<=120)) { // Button: 2
+                        betast(70, 10, 120, 60);
+                        updateStr('2');
+                    }
+                    if ((x>=130) && (x<=180))  // Button: 3
+                    {
+                    betast(130, 10, 180, 60);
+                    updateStr('3');
+                    }
+                    if ((x>=190) && (x<=240))  // Button: 4
+                    {
+                    betast(190, 10, 240, 60);
+                    updateStr('4');
+                    }
+                    if ((x>=250) && (x<=300))  // Button: 5
+                    {
+                    betast(250, 10, 300, 60);
+                    updateStr('5');
+                    }
+                }
+
+                if ((y>=70) && (y<=120)) { // Center row
+                    if ((x>=10) && (x<=60))  {// Button: 6
+                        betast(10, 70, 60, 120);
+                        updateStr('6');
+                    }
+                    if ((x>=70) && (x<=120))  // Button: 7
+                    {
+                    betast(70, 70, 120, 120);
+                    updateStr('7');
+                    }
+                    if ((x>=130) && (x<=180))  // Button: 8
+                    {
+                    betast(130, 70, 180, 120);
+                    updateStr('8');
+                    }
+                    if ((x>=190) && (x<=240))  // Button: 9
+                    {
+                    betast(190, 70, 240, 120);
+                    updateStr('9');
+                    }
+                    if ((x>=250) && (x<=300))  // Button: 0
+                    {
+                    betast(250, 70, 300, 120);
+                    updateStr('0');
+                    }
+                }
+
+                if ((y>=130) && (y<=180))  {// Upper row
+                    if ((x>=10) && (x<=150))  {// Button: Clear
+                        betast(10, 130, 150, 180);
+                        stCurrent[0]='\0';
+                        stCurrentLen=0;
+                        myGLCD.setColor(0, 0, 0);
+                        myGLCD.fillRect(0, 224, 319, 239);
+                        }
+                        if ((x>=160) && (x<=300))  // Button: Enter
+                        {
+                        betast(160, 130, 300, 180);
+                        if (stCurrentLen>0)
+                        {
+                            for (x=0; x<stCurrentLen+1; x++)
+                            {
+                            stLast[x]=stCurrent[x];
+                            }
+                            stCurrent[0]='\0';
+                            stCurrentLen=0;
+                            myGLCD.setColor(0, 0, 0);
+                            myGLCD.fillRect(0, 208, 319, 239);
+                            myGLCD.setColor(0, 255, 0);
+                            myGLCD.print(stLast, LEFT, 208);
+                            int number = atoi(stLast);
+                            plantenBakSettings1[bak][seizoen][variable] = number;
+                            
+                            stCurrent[2]="";
+                            stCurrentLen=0;
+                            stLast[2]=""; 
+                            tekenSettingsManipulatieScherm(bak); 
+                            
+                        }
+                else
+                {
+                    myGLCD.setColor(255, 0, 0);
+                    myGLCD.print("BUFFER EMPTY", CENTER, 192);
+                    delay(500);
+                    myGLCD.print("            ", CENTER, 192);
+                    delay(500);
+                    myGLCD.print("BUFFER EMPTY", CENTER, 192);
+                    delay(500);
+                    myGLCD.print("            ", CENTER, 192);
+                    myGLCD.setColor(0, 255, 0);
+                }
+                }
+            }
+            }
+        }
+    }
+
+    
 
 
 // Draw a red frame while a button is touched
     void betast(int x1, int y1, int x2, int y2) {
+        Serial.println("begin betast");
         myGLCD.setColor(VGA_GREEN);
         myGLCD.drawRoundRect (x1, y1, x2, y2);
         while (myTouch.dataAvailable())
@@ -1287,8 +1278,11 @@ void waitForIt(int x1, int y1, int x2, int y2)
     }
 
     void kiesPlantenBak() {
+        Serial.println("begin kiesplantenbak");
         int gekozenBak = 3; //3 is niet bestaandebestaande plantenbak
         if (currentPage == 1 && myTouch.dataAvailable()) {
+                    Serial.print("currentPage = ");
+        Serial.println(currentPage);
             myTouch.read();
             x=myTouch.getX();
             y=myTouch.getY();
@@ -1336,6 +1330,7 @@ void setup() {
 }
 
 void loop() {
+    Serial.println("begin main");
     RtcDateTime tijd = klok.getTime();
     plantenbak1.regelKlimaat(tijd, bakNummer1);
     plantenbak2.regelKlimaat(tijd, bakNummer2);
@@ -1349,6 +1344,9 @@ void loop() {
     Serial.print("Het getal (main) is:");
     Serial.println(klimaatDataNu[2][LUCHTVOCHTIGHEIDNU]);
     while ((minuutNu - minuut) < 1) {
+        Serial.println("while loop in main");
+        Serial.println(minuut);
+        Serial.println(minuutNu);
         touchScreen.kiesPlantenBak();
         RtcDateTime nieuweTijd = klok.getTime();
         minuutNu = nieuweTijd.Minute();
